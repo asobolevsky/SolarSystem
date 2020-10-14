d237a546b26fbf09508e05df57e7b4e88cca77c2
Aleksei Sobolevskii <aleksei.sobolevskii@sentiance.com>
2020-10-14 17:16:37 -0400

    Initial commit

---
+++ SolarSystem/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  SolarSystem
+//
+//  Created by Aleksei Sobolevskii on 2020-10-14.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        let scene = SCNScene(named: "SolarSystem.scnassets/SolarSystenScene.scn")!
+        sceneView.scene = scene
+    }
+    p. 48
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
